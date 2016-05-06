9164e6a354cec8b0ae4eb0ca70c4792f52b7b0ad
mvgouvea <marcoskilf@gmail.com>
2016-05-06 13:58:28 -0300

    Tela de resultado

---
+++ src/view/TelaResultado.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import javax.swing.JToolBar;
+
+public class TelaResultado extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaResultado frame = new TelaResultado();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaResultado() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		JToolBar toolBar = new JToolBar();
+		contentPane.add(toolBar, "name_40270681120676");
+	}
+
+}
